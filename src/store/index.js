618def4ee70470a5bf1fb9f733f65bed6665dda4
sun0wei <1810915429@qq.com>
2024-01-08 09:23:57 +0800

    temp

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const mainStore = defineStore("main", {
   state: () => {
     return {
-      imgLoadStatus: false, // 壁纸加载状态
+      imgLoadStatus: true, // 壁纸加载状态
       innerWidth: null, // 当前窗口宽度
       coverType: "0", // 壁纸种类
       siteStartShow: true, // 建站日期显示
@@ -11,7 +11,7 @@ export const mainStore = defineStore("main", {
       musicIsOk: false, // 音乐是否加载完成
       musicVolume: 0, // 音乐音量;
       musicOpenState: false, // 音乐面板开启状态
-      backgroundShow: true, // 壁纸展示状态
+      backgroundShow: false, // 壁纸展示状态
       boxOpenState: false, // 盒子开启状态
       mobileOpenState: false, // 移动端开启状态
       mobileFuncState: false, // 移动端功能区开启状态
